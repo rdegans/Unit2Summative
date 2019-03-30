241ee1a3a9e3fdfb0b77b14e3d05f991f44804e9
rdegans <38084642+rdegans@users.noreply.github.com>
2019-03-29 22:37:24 -0400

    Add files via upload

--- Contacts.cs
+++ Contacts.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Name: Riley de Gans
+ * Date: March 29th, 2019
+ * Description: A program that formats and stores contacts from a form
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +23,10 @@ namespace _184863Unit2Summative
 {
     public class Contacts
     {
-        public string firstName, lastName, emailAdress;
+        public string firstName, lastName, emailAdress; //declare variables for class
         public int yearBorn, monthBorn, dayBorn;
         /// <summary>
-        /// 
+        /// create a contact object
         /// </summary>
         /// <param name="fN"></param>
         /// <param name="lN"></param>
@@ -32,7 +36,7 @@ namespace _184863Unit2Summative
         /// <param name="eA"></param>
         public Contacts(string fN, string lN, int yB, int mB, int dB, string eA)
         {
-            firstName = fN;
+            firstName = fN; //assign variables for class
             lastName = lN;
             yearBorn = yB;
             monthBorn = mB;
@@ -40,29 +44,29 @@ namespace _184863Unit2Summative
             emailAdress = eA;
         }
         /// <summary>
-        /// 
+        /// read the data from the file contact.txt and check for various exceptions
         /// </summary>
-        public string[] ReadFromFile()
+        public string[] ReadFromFile() //method to read and return contact info from the text file contact.txt
         {
-            string[] savedContact = new string [6];
+            string[] savedContact = new string [6]; //declare the string array that will be returned
             try
             {
-                System.IO.StreamReader sr = new System.IO.StreamReader("contact.txt");
+                System.IO.StreamReader sr = new System.IO.StreamReader("contact.txt"); //create stream reader to read from text file
                 savedContact = new string[6];
                 try
                 {
-                    savedContact = sr.ReadLine().Split(',');
+                    savedContact = sr.ReadLine().Split(','); //attempt to split the data by commas
                 }
                 catch
                 {
-                    for (int i = 0; i < savedContact.Length; i++)
+                    for (int i = 0; i < savedContact.Length; i++) //if there are no commas, enter place holder data for the user and force them to add new data to the text file before closing
                     {
                         savedContact[i] = "Enter your data here";
                     }
                 }
                 sr.Close();
             }
-            catch (FileNotFoundException ex)
+            catch (FileNotFoundException ex) //if the file does not exist, do the same thing as if there are no commas
             {
                 for (int i = 0; i < savedContact.Length; i++)
                 {
@@ -72,32 +76,30 @@ namespace _184863Unit2Summative
             return savedContact;
         }
         /// <summary>
-        /// 
+        /// save the data to the file contact.txt and check for various exceptions
         /// </summary>
-        public void SaveToFile()
+        public void SaveToFile() //method to save the data to the text file contact.txt as a comma sepperated line
         {
-            try
+            try //attempt to performt the action
             {
-                System.IO.StreamWriter sw = new System.IO.StreamWriter("contact.txt");
-                sw.WriteLine(firstName + "," + lastName + "," + yearBorn + "," + monthBorn + "," + dayBorn + "," + emailAdress);
+                System.IO.StreamWriter sw = new System.IO.StreamWriter("contact.txt"); //create stream writer
+                sw.WriteLine(firstName + "," + lastName + "," + yearBorn + "," + monthBorn + "," + dayBorn + "," + emailAdress); //write the values from the class that the method is being used on
                 sw.Flush();
                 sw.Close();
             }
-            catch (FileNotFoundException /*e*/)
+            catch (FileNotFoundException) //exception for if the file does not exist
             {
-               // MessageBox.Show(e.ToString());
             }
         }
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public double getAge()
+        public double getAge() //method to get the age of the user from their entered birth day
         {
-
-            DateTime birthDay = new DateTime(yearBorn, monthBorn, dayBorn);
-            DateTime currentDay = DateTime.Today;
-            return Math.Floor(currentDay.Subtract(birthDay).Days / 365.25);
+            DateTime birthDay = new DateTime(yearBorn, monthBorn, dayBorn); //create date time class with the values the user gave
+            DateTime currentDay = DateTime.Today; //create a date time class for today
+            return Math.Floor(currentDay.Subtract(birthDay).Days / 365.25); //returns the age in years, by dividing the timespan in days by 3.25 and rounding down
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Name: Riley de Gans
+ * Date: March 29th, 2019
+ * Description: A program that formats and stores contacts from a form
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,21 +29,23 @@ namespace _184863Unit2Summative
         public MainWindow()
         {
             InitializeComponent();
-            string[] contactNew = new string[6];
-           try
+            string[] contactNew = new string[6]; //array to temporarily store the contacts
+           try //if there are six items in the data
             {
-                Contacts contactTemp = new Contacts("placeholder", "placeholder", 1, 1, 1, "place holder");
-                contactNew = contactTemp.ReadFromFile();
-                txtFirstName.Text = contactNew[0];
+                Contacts contactTemp = new Contacts("placeholder", "placeholder", 1, 1, 1, "place holder"); //create a place holder class to apply the read from file method to
+                //this was kind of a paradox, because we were populating the class with the readfrom file, but using the read from file on the class, so we had to make a placeholder
+                //we couldn't find another way to do it, it is only slightly less efficient, and it works, so we went with it
+                contactNew = contactTemp.ReadFromFile();//read from file
+                txtFirstName.Text = contactNew[0];//set the text boxes to the saved contacts
                 txtLastName.Text = contactNew[1];
                 txtYearBorn.Text = contactNew[2];
                 txtMonthBorn.Text = contactNew[3];
                 txtDayBorn.Text = contactNew[4];
                 txtEmailAdress.Text = contactNew[5];
             }
-            catch
+            catch //exception for if there are not six items in the data
             {
-                txtFirstName.Text = "Enter your data here";
+                txtFirstName.Text = "Enter your data here"; //fill the fields temporarily and force the user to fix the problem before being able to close the window
                 txtLastName.Text = "Enter your data here";
                 txtYearBorn.Text = "Enter your data here";
                 txtMonthBorn.Text = "Enter your data here";
@@ -47,62 +53,62 @@ namespace _184863Unit2Summative
                 txtEmailAdress.Text = "Enter your data here";
             }
         }
-        private void BtnGetAge_Click(object sender, RoutedEventArgs e)
+        private void BtnGetAge_Click(object sender, RoutedEventArgs e) //click event for get age button
         {
-            try
+            try //if the data is all in int form and proper date form
             {
-                int birthYear, birthMonth, birthDay;
-                int.TryParse(txtDayBorn.Text, out birthDay);
+                int birthYear, birthMonth, birthDay; //declare the variables for age
+                int.TryParse(txtDayBorn.Text, out birthDay); //convert to int
                 int.TryParse(txtMonthBorn.Text, out birthMonth);
                 int.TryParse(txtYearBorn.Text, out birthYear);
-                Contacts contactCurrent = new Contacts(txtFirstName.Text, txtLastName.Text, birthYear, birthMonth, birthDay, txtEmailAdress.Text);
-                MessageBox.Show("Your age is " + contactCurrent.getAge() + " years old");
+                Contacts contactCurrent = new Contacts(txtFirstName.Text, txtLastName.Text, birthYear, birthMonth, birthDay, txtEmailAdress.Text); //create a new class
+                MessageBox.Show("Your age is " + contactCurrent.getAge() + " years old"); //print the age using the get age method
             }
-            catch (Exception ex)
+            catch (Exception ex) //if the format of the data is wrong
             {
-                MessageBox.Show("Please enter valid data for all fields and only enter number values for year born, month born and day born");
+                MessageBox.Show("Please enter valid data for all fields and only enter number values for year born, month born and day born"); //prompt the user to fix it and do not run the method
             }
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) //event for when the user tries to close the window
         {
-            try
+            try //check for formatting errors in the data ex. negative dates, letters for dates
             {
-                int birthYear, birthMonth, birthDay;
-                int.TryParse(txtDayBorn.Text, out birthDay);
+                int birthYear, birthMonth, birthDay; //declare the variables
+                int.TryParse(txtDayBorn.Text, out birthDay); //convert to int
                 int.TryParse(txtMonthBorn.Text, out birthMonth);
                 int.TryParse(txtYearBorn.Text, out birthYear);
-                DateTime placeHolder = new DateTime(birthYear, birthMonth, birthDay);
-                Contacts contactFinal = new Contacts(txtFirstName.Text, txtLastName.Text, birthYear, birthMonth, birthDay, txtEmailAdress.Text);
-                string[] userInput = new string[6];
-                userInput[0] = birthYear.ToString();
+                DateTime placeHolder = new DateTime(birthYear, birthMonth, birthDay); //create a placeholder date time that will cause the catch if the dates are incorrect ex. year -1 or feb 30th
+                Contacts contactFinal = new Contacts(txtFirstName.Text, txtLastName.Text, birthYear, birthMonth, birthDay, txtEmailAdress.Text); //create a new class
+                string[] userInput = new string[6]; //string array to store the user input to be manipulated
+                userInput[0] = birthYear.ToString(); //populate the array
                 userInput[1] = birthMonth.ToString();
                 userInput[2] = birthDay.ToString();
                 userInput[3] = txtFirstName.Text;
                 userInput[4] = txtLastName.Text;
                 userInput[5] = txtEmailAdress.Text;
-                for (int i = 0; i<userInput.Length; i++)
+                for (int i = 0; i<userInput.Length; i++) //check each position in the array
                 {
-                    if (userInput[i].Length == 0)
+                    if (userInput[i].Length == 0) //check for data
                     {
                         e.Cancel = true;
-                        MessageBox.Show("Please fill in all fields");
+                        MessageBox.Show("Please fill in all fields"); //if the field is blank, prompt the user to fix it and don't let them close the window
                     }
-                    if (userInput[i].Contains(','))
+                    if (userInput[i].Contains(',')) //check for commas
                     {
                         e.Cancel = true;
-                        MessageBox.Show("Please do not enter commas");
+                        MessageBox.Show("Please do not enter commas"); //if there are commas, prompt the user to fix it and don't let them close the window
                     }
                     else
                     {
-                        contactFinal.SaveToFile();
+                        contactFinal.SaveToFile(); //if the user is cooperating and all the data is formatted correctly save the file as normal
                     }
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex) //if the fields are formatted incorectly
             {
                 MessageBox.Show("Please enter valid data for all fields and only enter number values for year born, month born and day born");
-                e.Cancel = true;
+                e.Cancel = true;//prompt the user to fix it and do not let them close the window
             }
         }
     }
